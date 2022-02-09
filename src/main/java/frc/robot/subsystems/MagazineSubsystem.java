bc5f50c13e2956df294dfb94c1744f462fa8517a
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2022-02-08 18:12:50 -0700

    Add lasershark functions

--- src/main/java/frc/robot/subsystems/MagazineSubsystem.java
+++ src/main/java/frc/robot/subsystems/MagazineSubsystem.java
@@ -2,12 +2,15 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.cuforge.libcu.Lasershark;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class MagazineSubsystem extends SubsystemBase {
     private TalonSRX magazineMotor = new TalonSRX(RobotMap.MAGAZINE_MOTOR);
+    private Lasershark bottomDetector = new Lasershark(0);
+    private Lasershark topDetector = new Lasershark(1);
     
     public MagazineSubsystem() {
 
@@ -23,5 +26,13 @@ public class MagazineSubsystem extends SubsystemBase {
     }
     public void magazineOff() {
         magazineMotor.set(ControlMode.PercentOutput, 0);
+    }
+
+    public boolean bottomDetector() {
+        return bottomDetector.getDistanceInches() > 0 && bottomDetector.getDistanceInches() < 1;
+    }
+    
+    public boolean topDetector() {
+        return topDetector.getDistanceInches() > 0 && topDetector.getDistanceInches() < 1;
     }
 }

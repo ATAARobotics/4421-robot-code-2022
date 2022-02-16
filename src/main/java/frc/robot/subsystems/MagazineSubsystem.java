3a9cb166ddb9684455046fecb840cbecbb537909
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2022-02-15 18:38:42 -0700

    Lasershark update in ultrasonic branch :)

--- src/main/java/frc/robot/subsystems/MagazineSubsystem.java
+++ src/main/java/frc/robot/subsystems/MagazineSubsystem.java
@@ -4,28 +4,21 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.cuforge.libcu.Lasershark;
 
-import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class MagazineSubsystem extends SubsystemBase {
-    //private Ultrasonic bottomDetector = new Ultrasonic(RobotMap.BOTTOM_DETECTOR[0], RobotMap.BOTTOM_DETECTOR[1]);
-    //private Ultrasonic topDetector = new Ultrasonic(RobotMap.TOP_DETECTOR[0], RobotMap.TOP_DETECTOR[1]);
     private Lasershark bottomDetector = new Lasershark(0);
     private Lasershark topDetector = new Lasershark(1);
     private TalonSRX magazineMotor = new TalonSRX(RobotMap.MAGAZINE_MOTOR);
 
     public MagazineSubsystem() {
-        /*Ultrasonic.setAutomaticMode(true);
-        bottomDetector.setEnabled(true);
-        topDetector.setEnabled(true);*/
+
     }
 
     @Override
     public void periodic() {
-        //SmartDashboard.putNumber("Bottom Detector", bottomDetector.getRangeMM());
-        //SmartDashboard.putNumber("Top Detector", topDetector.getRangeMM());
         SmartDashboard.putBoolean("Bottom Detector", bottomDetector());
         SmartDashboard.putNumber("Bottom Detector Range", bottomDetector.getDistanceInches());
         SmartDashboard.putBoolean("Top Detector", topDetector());
@@ -56,25 +49,4 @@ public class MagazineSubsystem extends SubsystemBase {
     public boolean bothDetectors() {
         return bottomDetector() && topDetector();
     }
-    
-
-    /*public boolean bottomDetectorInRange() {
-        return bottomDetector.getRangeMM() > RobotMap.INTAKE_RANGE[0] && bottomDetector.getRangeMM() < RobotMap.INTAKE_RANGE[1];
-    }
-    
-    public boolean topDetectorInRange() {
-        return topDetector.getRangeMM() > RobotMap.INTAKE_RANGE[0] && topDetector.getRangeMM() < RobotMap.INTAKE_RANGE[1];
-    }
-
-    public boolean bottomDetectorOnly() {
-        return bottomDetectorInRange() && !topDetectorInRange();
-    }
-    
-    public boolean topDetectorOnly() {
-        return topDetectorInRange() && !bottomDetectorInRange();
-    }
-
-    public boolean bothDetectors() {
-        return bottomDetectorInRange() && topDetectorInRange();
-    }*/
 }

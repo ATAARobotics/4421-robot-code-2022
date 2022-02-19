93fe2e79bd7d0a19dc66faf35237d2a554b966c0
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2022-02-19 16:32:01 -0700

    Remove logic from climb subsystem

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -6,31 +6,16 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
-public class ClimberSubsystem {
+public class ClimberSubsystem extends SubsystemBase {
     private CANSparkMax elevator = new CANSparkMax(RobotMap.CLIMB_MOTOR, MotorType.kBrushless);
     private DoubleSolenoid arm = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, RobotMap.CLIMB_ARM[0], RobotMap.CLIMB_ARM[1]);
-    private boolean armTilted = false;
     private double elevatorSpeed = 0.5;
-    private double elevatorSpeedChange = 0.1;
 
     public ClimberSubsystem() {
 
-    }
-    public void increaseSpeed() {
-        elevatorSpeed += elevatorSpeedChange;
-        if (elevatorSpeed > 1) {
-            elevatorSpeed = 1;
-        }
-    }
-    
-    public void decreaseSpeed() {
-        elevatorSpeed -= elevatorSpeedChange;
-        if (elevatorSpeed < 0) {
-            elevatorSpeed = 0;
-        }
     }
 
     public void climberUp() {
@@ -39,19 +24,6 @@ public class ClimberSubsystem {
 
     public void climberDown() {
         elevator.set(-elevatorSpeed);
-    }
-    public void climberDirectionEnable(int speed) {
-        elevator.set(elevatorSpeed*speed);
-        SmartDashboard.putNumber("climber speed", elevatorSpeed);
-    }
-
-    public void toggleArm() {
-        armTilted = !armTilted;
-        if (armTilted) {
-            armTilt();
-        } else {
-            armVertical();
-        }
     }
 
     public void armTilt() {

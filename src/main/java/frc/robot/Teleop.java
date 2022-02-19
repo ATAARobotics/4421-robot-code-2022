8843aef14234a9dfb2aa23061c9d8f2295279f0c
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2022-02-19 14:16:48 -0700

    Unused import

--- src/main/java/frc/robot/Teleop.java
+++ src/main/java/frc/robot/Teleop.java
@@ -3,7 +3,6 @@ package frc.robot;
 import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.cscore.VideoSink;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;

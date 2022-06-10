181796e44a1ab5a1a6346f2e704be5edac417f76
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2022-06-09 17:16:11 -0700

    Comment unused lines for low auto

--- src/main/java/frc/robot/commands/auto/TwoBallAutoQ1Low.java
+++ src/main/java/frc/robot/commands/auto/TwoBallAutoQ1Low.java
@@ -1,14 +1,17 @@
 package frc.robot.commands.auto;
-
+/* 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand; 
+
+import frc.robot.AutoPaths; */
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-import frc.robot.AutoPaths;
 import frc.robot.subsystems.*;
 
 public class TwoBallAutoQ1Low extends SequentialCommandGroup {
+    /*
     private final SwerveDriveSubsystem m_swerveDriveSubsystem;
     private final ShooterSubsystem m_shooterSubsystem;
     private final ClimbMotorSubsystem m_climbMotorSubsystem;
@@ -16,17 +19,18 @@ public class TwoBallAutoQ1Low extends SequentialCommandGroup {
     private final HoodSubsystem m_hoodSubsystem;
     private final IntakeSubsystem m_intakeSubsystem;
     private final MagazineSubsystem m_magazineSubsystem;
-    private AutoPaths autoPaths;
+    private AutoPaths autoPaths; */
 
     public TwoBallAutoQ1Low(SwerveDriveSubsystem swerveDriveSubsystem, ClimbMotorSubsystem climbMotorSubsystem, ClimbArmSubsystem climbArmSubsystem, IntakeSubsystem intakeSubsystem, HoodSubsystem hoodSubsystem, MagazineSubsystem magazineSubsystem, ShooterSubsystem shooterSubsystem) {
         addRequirements(swerveDriveSubsystem, climbMotorSubsystem, climbArmSubsystem, intakeSubsystem, hoodSubsystem, magazineSubsystem, shooterSubsystem);
+        /*
         m_swerveDriveSubsystem = swerveDriveSubsystem;
         m_climbArmSubsystem = climbArmSubsystem;
         m_climbMotorSubsystem = climbMotorSubsystem;
         m_shooterSubsystem = shooterSubsystem;
         m_magazineSubsystem = magazineSubsystem;
         m_hoodSubsystem = hoodSubsystem;
-        m_intakeSubsystem = intakeSubsystem;
+        m_intakeSubsystem = intakeSubsystem; */
         addCommands(
         );
     }

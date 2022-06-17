9bc6934ee0c5a58ce8f54285a8ecef9161141d39
Jacob-Guglielmin <jacobguglielmin@gmail.com>
2022-06-16 20:10:42 -0600

    add message for next person who works on this

--- src/main/java/frc/robot/Pigeon.java
+++ src/main/java/frc/robot/Pigeon.java
@@ -2,6 +2,8 @@ package frc.robot;
 
 import com.ctre.phoenix.sensors.Pigeon2;
 
+//TODO Whoever sees this message, this probably doesnt work - fix this before you do whatever you were planning on doing
+
 public class Pigeon {
 
     private Pigeon2 pigeon;

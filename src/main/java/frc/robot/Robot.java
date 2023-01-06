1c4c10a5879c5b70f73d4d8982fa05af985ad370
mateo <mateo.e.yajure@gmail.com>
2023-01-05 18:17:26 -0700

    fixed flywheel startup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,11 +1,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.auto.Straight;
 
 public class Robot extends TimedRobot {
 
@@ -101,7 +99,7 @@ public class Robot extends TimedRobot {
             m_autonomousCommand = null;
         }
         robotContainer.getSwerveDriveSubsystem().setBrakes(true);
-
+        robotContainer.getShooterSubsystem().shooterOff();
         robotContainer.getIntakeSubsystem().intakeOff();
         robotContainer.getShooterSubsystem().pidReset();
     }
